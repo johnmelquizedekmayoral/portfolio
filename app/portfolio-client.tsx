398a034ed93456f4652f2b35ab4337b9530bcc48
John Melquizedek Mayoral <johnmelquizedekmayoral@gmail.com>
2026-09-16 14:18:41 +0800

    Fix background overlay 2

--- app/portfolio-client.tsx
+++ app/portfolio-client.tsx
@@ -875,7 +875,7 @@ function GameBackground({
       )}
 
       {/* Slight readability layer */}
-      <div className="absolute inset-0 bg-white/25" />
+      <div className="absolute inset-0 bg-white/50" />
     </div>
   )
 }
