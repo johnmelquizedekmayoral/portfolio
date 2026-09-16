02be5f5eb5c273f6ea2887dcf4933630ef277809
John Melquizedek Mayoral <johnmelquizedekmayoral@gmail.com>
2026-09-16 14:02:00 +0800

    Fix background media query 3

--- app/portfolio-client.tsx
+++ app/portfolio-client.tsx
@@ -875,7 +875,7 @@ function GameBackground({
       )}
 
       {/* Slight readability layer */}
-      <div className="absolute inset-0 bg-white/10" />
+      <div className="absolute inset-0 bg-black/50" />
     </div>
   )
 }
